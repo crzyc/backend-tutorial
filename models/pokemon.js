b8f9473fc60b968ea8dbda3ff81087f88995b793
BuildTools <unconfigured@null.spigotmc.org>
2020-11-20 11:18:52 -0500

    Misc. bugfixes and changes.

---
+++ models/pokemon.js
@@ -0,0 +1,12 @@
+// Load required packages
+var mongoose = require('mongoose');
+
+// Define our Pokemon Schema
+var pokeSchema = new mongoose.Schema({
+  name: String,
+  type: String,
+  quantity: Number
+});
+
+// Export the Mongoose model
+module.exports = mongoose.model('Pokemon', pokeSchema);
--- server.js
+++ server.js
@@ -1,24 +1,54 @@
-// Get the packages we need
+// Load required packages
 var express = require('express');
-
-// Create our Express application
 var app = express();
-
-// Use environment defined port or 3000
 var port = process.env.PORT || 3000;
-
-// Create our Express router
 var router = express.Router();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var Pokemon = require('./models/pokemon');
 
 // Initial dummy route for testing
 // http://localhost:3000/api
 router.get('/', function(req, res) {
-  res.json('Your pokedex is nowhere near complete!');
+  res.json({"message": 'Your pokedex is empty!'});
 });
 
-// Register all our routes with /api
 app.use('/api', router);
 
-// Start the server
 app.listen(port);
-console.log('Insert PC system on port ' + port);
+console.log('Insert pokemon on port ' + port);
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/pokedex",{
+	useNewUrlParser: true, 
+	useUnifiedTopology: true,
+});
+
+// Use the body-parser package in our application
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// -- New Code Below Here -- //
+
+// Create a new route with the prefix /beers
+var pokeRoute = router.route('/pokemon');
+
+// Create endpoint /api/beers for POSTS
+pokeRoute.post(function(req, res) {
+  // Create a new instance of the Pokemon model
+  var pokemon = new pokemon();
+
+  // Set the beer properties that came from the POST data
+  pokemon.name = req.body.name;
+  pokemon.type = req.body.type;
+  pokemon.quantity = req.body.quantity;
+
+  // Save the beer and check for errors
+  pokemon.save(function(err) {
+    if (err)
+      res.send(err);
+
+    res.json({ message: 'Pokemon added to the PC Box!', data: pokemon });
+  });
+});
